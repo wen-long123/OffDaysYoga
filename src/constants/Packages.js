af68909628409d05f084f880ea21d1293d804dc3
wen-long123 <seahwenlong2@gmail.com>
2025-10-10 21:37:30 +0800

    changed packages name

--- src/constants/Packages.js
+++ src/constants/Packages.js
@@ -1,18 +1,18 @@
 export const packages = [
   {
-    name: "Single Drop",
+    name: "Taster Class",
     price: "$15",
     description: "Valid for 1 month",
     link: "https://www.momoyoga.com/offdaysyoga/member/order/create/choose-product",
   },
   {
-    name: "5 Class Pack",
+    name: "Five & Thrive",
     price: "$60",
     description: "Valid for 3 months",
     link: "https://www.momoyoga.com/offdaysyoga/member/order/create/choose-product",
   },
   {
-    name: "12 Class Pack",
+    name: "Full Bloom",
     price: "$120",
     description: "Valid for 6 months",
     link: "https://www.momoyoga.com/offdaysyoga/member/order/create/choose-product",
